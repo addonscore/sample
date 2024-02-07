78ebe96f43c62390304d5e48cdb658ce6d78330c
pimenlabs <pimenlabs@gmail.com>
2024-02-08 06:23:06 +0800

    first commit

---
+++ core/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <VRow>
+    <VCol
+      cols="12"
+      md="12"
+    >
+      <VCard title="Welcome">
+        <VCardText>
+          Core 
+        </VCardText>
+      </VCard>
+    </VCol>
+  
+  </VRow>
+</template>
+
+<route lang="yaml">
+  meta:
+    action: manage
+    subject: Client
+  </route>
+    
+  
